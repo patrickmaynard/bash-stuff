727e93bf506ff48bb173c4eb772afa55951325da
Patrick Maynard <patrickmaynard.bs@gmail.com>
2023-02-22 13:24:11 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 apt-get install -y qgis gimp
+
+# Now copy your documents from the old machine to the new machine via your thumb drive or backup drive.
+
+# Then (assuming you used the thumb drive) re-download your Linux distribution of choice and use Balena Etcher to flash the drive again.
