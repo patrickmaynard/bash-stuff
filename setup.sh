60aeab7425a21beebc7d60ea9c085ff106c50f58
Patrick Maynard <patrickmaynard.bs@gmail.com>
2023-02-24 16:07:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,5 @@ apt-get install -y qgis gimp
 # Now copy your documents from the old machine to the new machine via your thumb drive or backup drive.
 
 # Then (assuming you used the thumb drive) re-download your Linux distribution of choice and use Balena Etcher to flash the drive again.
+
+# Then reinstall PHPStorm, using a Google Drive document to hang onto the necessary user key.
