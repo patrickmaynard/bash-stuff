381f7e0392da1336b145eb86ef8c714bd6bd65e1
Patrick Maynard <patrickmaynard.bs@gmail.com>
2023-07-11 21:17:41 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@
 # Use f8 to boot from USB and install Linux. Then ...
 
 sudo apt-get install -y qgis gimp neovim python3 python3-pip screen git \
-    curl wget musescore3 whois tldr gnupg kgpg php-cli sqlite3 composer
+    curl wget musescore3 whois tldr gnupg kgpg php-cli sqlite3 composer \
+    php-xml php-curl
 
 # Now we configure the terminal to copy automatically on select, like iTerm2 does, per https://superuser.com/a/1721880
 
